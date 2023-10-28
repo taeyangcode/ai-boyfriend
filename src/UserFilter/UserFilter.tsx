e953273476e3c6268445b4317b064858ddda8240
jengoc415 <jengoc415@gmail.com>
2023-10-28 00:41:20 -0700

    Basic UI improvements

--- src/UserFilter/UserFilter.tsx
+++ src/UserFilter/UserFilter.tsx
@@ -32,80 +32,91 @@ function UserFilter() {
     }, []);
 
     return (
-        <div>
-            <div>
+        <div className="bg-gray-100 p-8 rounded-lg shadow-md">
+            <h2 className="text-2xl font-semibold mb-4">Restaurant Preferences</h2>
+
+            <div className="mb-4">
                 <div>Longitude: {currentPosition?.coords.longitude}</div>
                 <div>Latitude: {currentPosition?.coords.latitude}</div>
             </div>
 
-            <div>
-                <div className="bg-red-100 p-8 shadow-md rounded-lg">
-                    <h2 className="text-2xl font-semibold mb-4">Restaurant Preferences</h2>
+            <form>
+                <div className="mb-4">
+                    <label className="block mb-2">How much are you willing to spend on the meal? </label>
+                    <input
+                        type="text"
+                        value={budget}
+                        onChange={(e) => setBudget(e.target.value)}
+                        className="border border-gray-300 px-2 py-1 rounded w-full"
+                    />
                 </div>
-
-                <form>
-                    <div>
-                        <label>How much are you willing to spend on the meal? </label>
-                        <input type="text" value={budget} onChange={(e) => setBudget(e.target.value)} />
+                <div className="mb-4">
+                    <label className="block mb-2">How far are you willing to travel? </label>
+                    <input
+                        type="text"
+                        value={distance}
+                        onChange={(e) => setDistance(e.target.value)}
+                        className="border border-gray-300 px-2 py-1 rounded w-full"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block mb-2">What time do you want to visit the restaurant? </label>
+                    <input
+                        type="text"
+                        value={time}
+                        onChange={(e) => setTime(e.target.value)}
+                        className="border border-gray-300 px-2 py-1 rounded w-full"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block mb-2">Do you have any dietary preferences?</label>
+                    <div className="space-y-2">
+                        <label className="flex items-center">
+                            <input
+                                type="checkbox"
+                                value="Vegetarian"
+                                checked={dietaryPreferences.includes("Vegetarian")}
+                                onChange={handleDietaryPreferenceChange}
+                                className="mr-2"
+                            />
+                            Vegetarian
+                        </label>
+                        <label className="flex items-center">
+                            <input
+                                type="checkbox"
+                                value="Vegan"
+                                checked={dietaryPreferences.includes("Vegan")}
+                                onChange={handleDietaryPreferenceChange}
+                                className="mr-2"
+                            />
+                            Vegan
+                        </label>
+                        <label className="flex items-center">
+                            <input
+                                type="checkbox"
+                                value="Seafood"
+                                checked={dietaryPreferences.includes("Seafood")}
+                                onChange={handleDietaryPreferenceChange}
+                                className="mr-2"
+                            />
+                            Seafood
+                        </label>
+                        <label className="flex items-center">
+                            <input
+                                type="checkbox"
+                                value="Gluten-free"
+                                checked={dietaryPreferences.includes("Gluten-free")}
+                                onChange={handleDietaryPreferenceChange}
+                                className="mr-2"
+                            />
+                            Gluten-free
+                        </label>
                     </div>
-                    <div>
-                        <label>How far are you willing to travel? </label>
-                        <input type="text" value={distance} onChange={(e) => setDistance(e.target.value)} />
-                    </div>
-                    <div>
-                        <label>What time do you want to visit the restaurant? </label>
-                        <input type="text" value={time} onChange={(e) => setTime(e.target.value)} />
-                    </div>
-                    <div>
-                        <label>Do you have any dietary preferences?</label>
-                        <div>
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    value="Vegetarian"
-                                    checked={dietaryPreferences.includes("Vegetarian")}
-                                    onChange={handleDietaryPreferenceChange}
-                                />{" "}
-                                Vegetarian
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    value="Vegan"
-                                    checked={dietaryPreferences.includes("Vegan")}
-                                    onChange={handleDietaryPreferenceChange}
-                                />{" "}
-                                Vegan
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    value="Seafood"
-                                    checked={dietaryPreferences.includes("Seafood")}
-                                    onChange={handleDietaryPreferenceChange}
-                                />{" "}
-                                Seafood
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    value="Gluten-free"
-                                    checked={dietaryPreferences.includes("Gluten-free")}
-                                    onChange={handleDietaryPreferenceChange}
-                                />{" "}
-                                Gluten-free
-                            </label>
-                        </div>
-                    </div>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
+                </div>
+                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
+                    Submit
+                </button>
+            </form>
         </div>
     );
 }
