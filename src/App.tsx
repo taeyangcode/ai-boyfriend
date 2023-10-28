a30fc539bc6f4bcc2e22365d25790e4854756ef0
jengoc415 <jengoc415@gmail.com>
2023-10-28 14:59:50 -0700

    Added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
-import { useState } from "react";
-import UserFilter from "./UserFilter/UserFilter";
-import NotificationHandler from "./Notification/Notification";
+import { useState } from 'react'
+import UserFilter from './UserFilter/UserFilter'
+import NotificationHandler from './Notification/Notification'
 
 function App() {
-    const [notifications, setNotifications] = useState<Array<NotificationType>>([]);
+    const [notifications, setNotifications] = useState<Array<NotificationType>>(
+        []
+    )
+    const [selectedPage, setSelectedPage] = useState<Page>('preference')
+
+    const changePage = (newPage: Page) => {
+        setSelectedPage(newPage)
+    }
+
+    const renderPage = () => {
+        switch (selectedPage) {
+            case 'preference':
+                return (
+                    <UserFilter
+                        notifications={notifications}
+                        setNotifications={setNotifications}
+                        changePage={changePage}
+                    />
+                )
+            // case 'questionnaire':
+            //     return <Questionnaire />
+            // case 'result':
+            //     return <Result />
+            default:
+                return (
+                    <>
+                        <div>Page not found</div>
+                    </>
+                )
+        }
+    }
 
     return (
         <>
             <NotificationHandler notifications={notifications} />
-            <UserFilter notifications={notifications} setNotifications={setNotifications} />;
+            {renderPage()}
         </>
-    );
+    )
 }
 
-export default App;
+export default App
---
+++ src/Questionnaire/Questionnaire.tsx
@@ -0,0 +1,11 @@
+import './../index.css'
+
+function Questionnaire() {
+    return (
+        <div className="rounded-lg bg-gray-100 p-8 shadow-md">
+            <h2 className="mb-4 text-2xl font-semibold">Pls choose more</h2>
+        </div>
+    )
+}
+
+export default Questionnaire
