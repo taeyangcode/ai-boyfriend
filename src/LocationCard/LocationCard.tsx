9d14a91c6e08f6a1817384bc81bb45a91098d0a4
bryanwonggy <bryanwong040500@gmail.com>
2023-10-29 11:48:19 -0700

    open new tab

--- src/LocationCard/LocationCard.tsx
+++ src/LocationCard/LocationCard.tsx
@@ -24,6 +24,8 @@ function LocationCard({ businessData }: Props) {
                 <div className="flex justify-center px-1 py-2">
                     <a
                         href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
                         className="mb-2 mr-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                         See on Yelp
